0c48d500230784def6e1f1909403f8d3bae360cc
TouhedurRahman <touhedur.cse@gmail.com>
2023-03-05 23:56:10 +0600

    read data from mongodb added bug fixed

--- index.js
+++ index.js
@@ -11,13 +11,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-/***************************************
-
-    username: doctors_portal
-    password: xNXfcWEqtIH4g8bu
-
- ***************************************/
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yrcmf.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
