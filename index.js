2fd1d6b78a88c49013c8e6b148f3325755abe1c5
TouhedurRahman <touhedur.cse@gmail.com>
2023-03-05 23:55:08 +0600

    read data from mongodb added

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
 const port = process.env.PORT || 5000;
 
 const app = express();
 
+// middleware
 app.use(cors());
 app.use(express.json());
+
+/***************************************
+
+    username: doctors_portal
+    password: xNXfcWEqtIH4g8bu
+
+ ***************************************/
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yrcmf.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const appointmentOptionsCollection = client.db('doctors_portal').collection('appointmentOptions');
+
+        app.get('/appointmentOptions', async (req, res) => {
+            const query = {};
+            const options = await appointmentOptionsCollection.find(query).toArray();
+            res.send(options);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
 
 app.get('/', async (req, res) => {
     res.send("doctor portal server is running");
