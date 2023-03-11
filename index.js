b8bed0de059845c714ca77a711547b81acf15a53
TouhedurRahman <touhedur.cse@gmail.com>
2023-03-11 14:54:35 +0600

    security updated

--- index.js
+++ index.js
@@ -113,6 +113,12 @@ async function run() {
             res.status(403).send({ accessToken: '' });
         })
 
+        app.get('/users', async (req, res) => {
+            const query = {};
+            const users = await usersCollection.find(query).toArray();
+            res.send(users);
+        })
+
         app.post('/users', async (req, res) => {
             const user = req.body;
             // console.log(user);
